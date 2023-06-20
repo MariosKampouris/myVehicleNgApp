5542fb1885e61d7a53647dcffda2882ef1a9e312
MariosKampouris <marioswow20@gmail.com>
2023-06-20 23:37:12 +0300

    Implemented Logout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +8,21 @@ import { AuthService } from '../auth/auth.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  isAuthenticated = false;
+  private userSub: Subscription;
+
   constructor(private authService: AuthService){
     
+  }
+
+  ngOnInit():void {
+    this.authService.user.subscribe(user => {
+      this.isAuthenticated = !user ? false : true;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
   }
 
   onLogout(){
