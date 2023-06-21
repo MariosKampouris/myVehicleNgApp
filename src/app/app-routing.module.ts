9589b2a891416fc1bb624f27f064a39d42f103e6
MariosKampouris <marioswow20@gmail.com>
2023-06-21 14:15:52 +0300

    Implemented Authorization Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,16 @@ import { BrandFavoritesComponent } from './brand-favorites/brand-favorites.compo
 import { AuthComponent } from './auth/auth.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component : HomeComponent},
-  {path: 'myCars', component : MyCarsComponent, children: []},
-  {path: 'carGallery', component : CarGalleryComponent},
-  {path: 'brandFavorites', component : BrandFavoritesComponent},
+  {path: 'home', component : HomeComponent, canActivate: [AuthGuard]},
+  {path: 'myCars', component : MyCarsComponent, canActivate: [AuthGuard], children: []},
+  {path: 'carGallery', component : CarGalleryComponent, canActivate: [AuthGuard]}, 
+  {path: 'brandFavorites', component : BrandFavoritesComponent , canActivate: [AuthGuard]},
   {path: 'auth', component: AuthComponent},
-  {path: 'contact', component: ContactComponent}
+  {path: 'contact', component: ContactComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
